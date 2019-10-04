93da48e7ae757eef1b9c4016243a605fc3c8bb84
anissfadel123 <anissfadel123@yahoo.com>
2019-10-04 00:00:58 -0400

    Commit: revised algorithm

--- src/MondrianArt.java
+++ src/MondrianArt.java
@@ -1,10 +1,15 @@
+// Aniss Fadel
+// CISC 4900
+// Task: to write a program that uses recursion to generate pseudo-random art in a Mondrian style.
+// MondrianArt.class
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.util.Random;
-//change split to splitRegion
+
 public class MondrianArt {
     private int canvasHeight;
     private int canvasWidth;
@@ -12,68 +17,80 @@ public class MondrianArt {
     private Graphics2D g2d;
     private File file;
     Random random;
-    MondrianArt(int width, int height){
+    MondrianArt(int width, int height, String name){
+
         this.canvasWidth = width;
         this.canvasHeight = height;
         bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         g2d = bufferedImage.createGraphics();
         random = new Random();
 
-        //sets the color of the whole canvas to white
-        g2d.setColor(Color.white);
-        g2d.fillRect(0, 0,canvasWidth, canvasHeight);
 
-        //set upper boundary and left boundary black
-        g2d.setColor(Color.black);
-        g2d.drawLine(0, 0, width,0);
-        g2d.drawLine(0, 1, 0,height);
-
-        split(0, 0, canvasWidth, canvasHeight);
-        printCanvas("canvasArt.png");
+        splitRegion(0, 0, canvasWidth, canvasHeight);
+        printCanvas(name);
         g2d.dispose();
 
     }
-    private void split(int regionX, int regionY, int regionWidth, int regionHeight){
+    // Generates psuedo-random art in a Mondrian style.
+    // Splits region depending on regionWidth && regionHeight
+    // recursively calls the splitted regions
+    // eventually colors the regions (white, blue, red, yellow)
+    private void splitRegion(int regionX, int regionY, int regionWidth, int regionHeight){
         if(regionWidth > canvasWidth/2 && regionHeight > canvasHeight/2){
 
-//            // draws black horizontal line on region
-//            int ranY = random.nextInt(regionHeight-1)+regionY+1;   // ranY: [1, regionHeight-1]
-//            g2d.drawLine(regionX, ranY, regionX+regionWidth-1, ranY);
-//
-//            // draws black vertical line on region
-//            int ranX = random.nextInt(regionWidth-1)+regionX+1;  // ranX: [1, regionWidth-1]
-//            g2d.drawLine(ranX, regionY, ranX, regionY+regionHeight-1);
             int ranY = drawHorizontalLine(regionX, regionY, regionWidth, regionHeight);
             int ranX = drawVerticalLine(regionX, regionY, regionWidth, regionHeight);
 
             //up-left region
-            split(regionX, regionY, ranX-regionX, ranY-regionY);
+            splitRegion(regionX, regionY, ranX-regionX, ranY-regionY);
             //up-right region
-            split(ranX, regionY,regionX+regionWidth-ranX, ranY-regionY);
+            splitRegion(ranX, regionY,regionX+regionWidth-ranX, ranY-regionY);
             //down-left region
-            split(regionX, ranY, ranX-regionX, regionY+regionHeight-ranY);
+            splitRegion(regionX, ranY, ranX-regionX, regionY+regionHeight-ranY);
             //down-right region
-            split(ranX, ranY, regionX+regionWidth-ranX, regionY+regionHeight-ranY);
+            splitRegion(ranX, ranY, regionX+regionWidth-ranX, regionY+regionHeight-ranY);
 
         }
         else if(regionWidth>canvasWidth/2){
-//            // draws black vertical line on region at possible random location
-//            int ranX = random.nextInt(regionHeight-1)+1;  // ranX: [1, regionHeight-1]
-//            g2d.drawLine(ranX, regionY, ranX, regionHeight);
+
             int ranX = drawVerticalLine(regionX, regionY, regionWidth, regionHeight);
 
-            split(regionX, regionY, ranX-regionX, regionHeight);
-            split(ranX, regionY, regionX+regionWidth-ranX,regionHeight);
+            splitRegion(regionX, regionY, ranX-regionX, regionHeight);
+            splitRegion(ranX, regionY, regionX+regionWidth-ranX,regionHeight);
 
         }
         else if(regionHeight > canvasHeight/2){
-//            // draws black horizontal line on region at possible random location
-//            int ranY = random.nextInt(regionWidth-1)+1;   // ranY: [1, regionWidth-1]
-//            g2d.drawLine(regionX, ranY, regionWidth-1, ranY);
             int ranY = drawHorizontalLine(regionX, regionY, regionWidth, regionHeight);
 
-            split(regionX, regionY, regionWidth, ranY-regionY);
-            split(regionX, ranY, regionWidth, regionY+regionHeight-ranY);
+            splitRegion(regionX, regionY, regionWidth, ranY-regionY);
+            splitRegion(regionX, ranY, regionWidth, regionY+regionHeight-ranY);
+        }
+        else if(regionHeight > 50 && regionWidth > 50){
+            int ranY = drawHorizontalLine(regionX, regionY, regionWidth, regionHeight);
+            int ranX = drawVerticalLine(regionX, regionY, regionWidth, regionHeight);
+
+            //up-left region
+            splitRegion(regionX, regionY, ranX-regionX, ranY-regionY);
+            //up-right region
+            splitRegion(ranX, regionY,regionX+regionWidth-ranX, ranY-regionY);
+            //down-left region
+            splitRegion(regionX, ranY, ranX-regionX, regionY+regionHeight-ranY);
+            //down-right region
+            splitRegion(ranX, ranY, regionX+regionWidth-ranX, regionY+regionHeight-ranY);
+        }
+        else if(regionWidth>50){
+
+            int ranX = drawVerticalLine(regionX, regionY, regionWidth, regionHeight);
+
+            splitRegion(regionX, regionY, ranX-regionX, regionHeight);
+            splitRegion(ranX, regionY, regionX+regionWidth-ranX,regionHeight);
+
+        }
+        else if(regionHeight > 50){
+            int ranY = drawHorizontalLine(regionX, regionY, regionWidth, regionHeight);
+
+            splitRegion(regionX, regionY, regionWidth, ranY-regionY);
+            splitRegion(regionX, ranY, regionWidth, regionY+regionHeight-ranY);
         }
         else{
             //chooses randomly white or colored (0 = white, 1 = colored)
@@ -96,8 +113,8 @@ public class MondrianArt {
         }
 
     }
-    // draws a vertical line on the region at a possible location
-    // returns the x-axis of the vertical line
+    // Draws a vertical line on the region at a possible location.
+    // Returns the x-axis of the vertical line
     public int drawVerticalLine(int x, int y, int width, int height){
         // draws black vertical line on region
         g2d.setColor(Color.black);
@@ -106,8 +123,8 @@ public class MondrianArt {
 
         return ranX;
     }
-    // draws a horizontal line on the region at a possible location
-    // returns the y-axis of the drawn horizontal line
+    // Draws a horizontal line on the region at a possible location.
+    // Returns the y-axis of the drawn horizontal line
     public int drawHorizontalLine(int x, int y, int width, int height){
         // draws black horizontal line on region
         g2d.setColor(Color.black);
@@ -117,7 +134,16 @@ public class MondrianArt {
         return ranY;
 
     }
+    // Prints the canvas into a png file
     public void printCanvas(String fileName){
+        //sets black boarder on canvas
+        g2d.setColor(Color.black);
+        g2d.drawLine(0, 0, canvasWidth,0);
+        g2d.drawLine(0, canvasHeight-1, canvasWidth,canvasHeight-1);
+        g2d.drawLine(0, 1, 0,canvasHeight);
+        g2d.drawLine(canvasWidth-1, 1, canvasWidth-1,canvasHeight);
+
+
         file = new File(fileName);
         try {
             ImageIO.write(bufferedImage, "png", file);
@@ -128,7 +154,10 @@ public class MondrianArt {
 
     }
     public static void main(String[] args) {
-        new MondrianArt(700,700);
+        new MondrianArt(700,700, "canvasArt1.png");
+        new MondrianArt(400,700, "canvasArt2.png");
+        new MondrianArt(900,700, "canvasArt3.png");
+
 
     }
 
